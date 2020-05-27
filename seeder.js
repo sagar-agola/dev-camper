797d11cb5ae55be8d2477246c12117feea2f2615
sagar-agola <n3o735@gmail.com>
2020-05-27 05:43:40 -0700

    added database seeder

---
+++ seeder.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const env = require('dotenv');
+
+// Load configuration variables
+env.config({ path: './config/config.env' });
+
+const Bootcamp = require('./models/Bootcamp');
+const connectDb = require('./config/db');
+
+// connect to mongodb
+connectDb();
+
+// read json data file
+const bootcamps = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// import data
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+        console.log('Data Imported...'.green.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(`${error}.bgWhite.red`);
+    }
+}
+
+// delete data
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+        console.log('Data deleted...'.red.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(`${error}.bgWhite.red`);
+    }
+}
+
+// read arguments
+if (process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+}
