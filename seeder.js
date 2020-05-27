aebb2519705fd1b8b915c7fae512cd7a333a01fb
sagar-agola <n3o735@gmail.com>
2020-05-27 05:53:46 -0700

    fixed bug in import functio in seeder file (used insertMany function insted of create)

--- seeder.js
+++ seeder.js
@@ -20,7 +20,7 @@ const bootcamps = JSON.parse(
 // import data
 const importData = async () => {
     try {
-        await Bootcamp.create(bootcamps);
+        await Bootcamp.insertMany(bootcamps);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch (error) {
@@ -32,7 +32,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
-        console.log('Data deleted...'.red.inverse);
+        console.log('Data deleted...'.bgWhite.red);
         process.exit();
     } catch (error) {
         console.log(`${error}.bgWhite.red`);
